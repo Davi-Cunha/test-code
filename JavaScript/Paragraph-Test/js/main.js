a98e969040c8dbe220a344ab02323bd4157a670a
Davi-Cunha <davipccunhasilva@hotmail.com>
2018-06-30 18:03:06 -0300

    Paragraph-Test added.

--- JavaScript/Paragraph-Test/js/main.js
+++ JavaScript/Paragraph-Test/js/main.js
@@ -5,3 +5,26 @@ function changeBackgroundColor(id) {
         paragraphs[i].style.backgroundColor = "lightGreen";
     }
 }
+
+
+var btnEveryP = document.querySelector(".btnEveryP");
+btnEveryP.addEventListener('click', function() {
+    var p = document.querySelector("#p1");
+    p.style.backgroundColor = "lightBlue";
+
+    p = document.querySelector("#p2");
+    p.style.backgroundColor = "indianRed";
+    p.style.border = " 3px dashed lightGreen"
+    p.style.textAlign = "center";
+    p.style.color = "lightGreen";
+
+    p = document.querySelector("#p3");
+    p.style.marginLeft = "100px"
+    p.innerHTML = "This Paragraph has moved 100px to the right";
+
+    p = document.querySelector("#p4");
+    p.style.fontSize = "25px";
+    p.style.fontFamily = "cursive";
+    p.style.color = "red";
+
+});
