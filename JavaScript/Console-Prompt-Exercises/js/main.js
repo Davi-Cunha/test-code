c7737bb45467a1b47b3a13fe404904ad67f3bfad
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-07-06 00:43:00 -0300

    C files added.

---
+++ JavaScript/Console-Prompt-Exercises/js/main.js
@@ -0,0 +1,8 @@
+var a, b, soma;
+
+console.log("Digite dois número para somar: ");
+var a = Number (prompt());
+var b = Number (prompt());
+soma = a + b;
+
+console.log("Soma de " + a + " + " + b + " = " + soma);
--- JavaScript/Drag-n-drop-Test/js/main.js
+++ JavaScript/Drag-n-drop-Test/js/main.js
@@ -1,7 +1,15 @@
-var iBrowsers = document.querySelectorAll("i");
-iBrowsers.forEach(function(i) {
+var iBrowsers1 = document.querySelectorAll("#browsersList1 i");
+iBrowsers1.forEach(function(i) {
     i.addEventListener('click', function() {
-        var coolBrowser = document.querySelector("#browsers")
+        var coolBrowser = document.querySelector("#browsers1")
         coolBrowser.appendChild(i);
+        i.onclick = null;
     });
+});
+
+var iBrowsers2 = document.querySelectorAll("#browsersList2 i");
+iBrowsers2.forEach(function(i) {
+    i.addEventListener('dragstart', function(target, evt) {
+        evt.dataTransfer.setData("#browsers2", target.id);
+    }, false);
 });
