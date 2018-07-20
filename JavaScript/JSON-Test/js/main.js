286681e6151321629abb69419ca4de09bdec2117
Davi-Cunha <davi.silva.1019097@sga.pucminas.br>
2018-07-20 20:40:58 -0300

    'Contact-Manager-Test' added.

--- JavaScript/JSON-Test/js/main.js
+++ JavaScript/JSON-Test/js/main.js
@@ -1,3 +1,5 @@
+// LOCAL OBJECT ACCESS
+
 // Object Array
 let anArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 // Parses the object 'anArray' into a JSON String
@@ -5,7 +7,7 @@ let JSONFormat = JSON.stringify(anArray);
 // Complex JavaScript object
 let complexObject = {
     name: 'Metallica',
-    album:[
+    album: [
         {
             name: "Master of Puppets",
             year: 1986
@@ -17,13 +19,13 @@ let complexObject = {
     ]
 };
 
-console.log(typeof(anArray))
+console.log(typeof (anArray))
 console.log(anArray);
-console.log(typeof(JSONFormat))
+console.log(typeof (JSONFormat))
 console.log(JSONFormat);
 
 JSONFormat = JSON.stringify(complexObject);
-console.log(typeof(JSONFormat));
+console.log(typeof (JSONFormat));
 console.log(JSONFormat);
 
 // Parses JSON which is a JSON String into a JavaScript Object and allows its properties to be accessed 
@@ -43,15 +45,48 @@ function search() {
     // On JSON file load do something
     xhr.onload = (e) => {
         let users = JSON.parse(xhr.response);
-        console.log(users[0]);
-        console.log(JSON.stringify(users[0]))
+        displayUsersAsTable(users);
     }
-
     xhr.send();
+}
 
+// Advanced Search
+function advancedSearch() {
+    let queryURL = "https://jsonplaceholder.typicode.com/users";
 
+    fetch(queryURL).then((response) => {
+        // response.json() returns a json string, returning it will convert it
+        // to a pure JavaScript object for the next then's callback
+        return response.json();
+    }).then((users) => {
+        // users is a JavaScript object here
+        displayUsersAsTable(users);
+    }).catch((err) => {
+        console.log("Error during fetching: " + err.message);
+    });
 }
 
 function displayUsersAsTable(users) {
+    let userDiv = document.querySelector("#users");
+    let table = document.createElement("table");
+    let row = table.insertRow();
 
+    userDiv.innerHTML = "";
+
+    row.innerHTML += "<th>Name</th><th>Username</th><th>Email</th>"
+    users.forEach((currentUser) => {
+        console.log(currentUser.name);
+        let row2 = table.insertRow();
+        row2.innerHTML += "<td>" + currentUser.name + "</td><td>" + currentUser.username + "</td><td>" + currentUser.email + "</td>";
+    });
+    userDiv.appendChild(table);
 }
+
+// LOCAL STORAGE 
+
+localStorage.lastName = "Bunny";
+localStorage.firstName = "Bugs";
+localStorage.location = "Earth";
+sessionStorage.lastName = "Bunny";
+sessionStorage.firstName = "Bugs";
+sessionStorage.location = "Earth";
